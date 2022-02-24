a8f3e4fbd2fbe25c1c644ad192ed7c5a5bb9c5a0
sw0rd0fg0d <100371862+sw0rd0fg0d@users.noreply.github.com>
2022-02-24 09:02:07 -0800

    Add files via upload
    TAX_RATE = 0.2
    STANDARD_DEDUCTION = 10000
    DEPENDENT_DEDUCTION = 2000
    STANDARD_DEDUCTION = 10000.00
    grossincome= input("input gross income: ")
    grossincome = float(grossincome)
    numberofdependents= input("input number of dependents: ")
    numberofdependents= float(numberofdependents)
    dependentDeduction= float(numberofdependents * DEPENDENT_DEDUCTION)
    grossIncomeAfterDeduction= float(grossincome - (dependentDeduction + STANDARD_DEDUCTION
    print("the gross income after tax is", incometax, "dollars.")

---
+++ lab6.py
@@ -0,0 +1,16 @@
+TAX_RATE = 0.2
+STANDARD_DEDUCTION = 10000
+DEPENDENT_DEDUCTION = 2000
+STANDARD_DEDUCTION = 10000.00
+grossincome= input("input gross income: ")
+grossincome = float(grossincome)
+numberofdependents= input("input number of dependents: ")
+numberofdependents= float(numberofdependents)
+dependentDeduction= float(numberofdependents * DEPENDENT_DEDUCTION)
+grossIncomeAfterDeduction= float(grossincome - (dependentDeduction + STANDARD_DEDUCTION
+print("the gross income after tax is", incometax, "dollars.")                                 
+                                
+                                 
+
+
+
